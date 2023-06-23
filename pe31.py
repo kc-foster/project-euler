c524b448834d34cf2684d5ce7d3f4235e5abf8ca
aridmango28 <caseyfoster325@gmail.com>
2023-06-23 15:20:24 -0400

    pe31 update 3

--- pe31.py
+++ pe31.py
@@ -3,9 +3,7 @@
 # Work in Progress
 
 import pdb
-from array import array
 
-unique_seqs = [[]]
 limit = 200
 
 x_inc = 200
@@ -17,67 +15,35 @@ d_inc = 5
 e_inc = 2
 f_inc = 1
 
-x = y = a = b = c = d = e = f = 0
+count = 0
+for x_sum in range(0, limit + 1, x_inc):
 
-for x_sum in range(0, limit, x_inc):
-
-	y = 0
-
-	for y_sum in range(0, limit - x_sum, y_inc):
-
-		a = 0
+	for y_sum in range(0, (limit - x_sum) + 1, y_inc):
 
 		a_start = x_sum + y_sum
 
-		for a_sum in range(0, limit - a_start, a_inc):
-
-			b = 0
+		for a_sum in range(0, (limit - a_start) + 1, a_inc):
 
 			b_start = x_sum + y_sum + a_sum
 
-			for b_sum in range(0, limit - b_start, b_inc):
-
-				c = 0
+			for b_sum in range(0, (limit - b_start) + 1, b_inc):
 
 				c_start = x_sum + y_sum + a_sum + b_sum
 
-				for c_sum in range(0, limit - c_start, c_inc):
-
-					d = 0
+				for c_sum in range(0, (limit - c_start) + 1, c_inc):
 
 					d_start = x_sum + y_sum + a_sum + b_sum + c_sum
 
-					for d_sum in range(0, limit - d_start, d_inc):
-
-						e = 0
+					for d_sum in range(0, (limit - d_start) + 1, d_inc):
 
 						e_start = x_sum + y_sum + a_sum + b_sum + c_sum + d_sum
 
-						for e_sum in range(0, limit - e_start, e_inc):
+						for e_sum in range(0, (limit - e_start) + 1, e_inc):
 
-							if (limit - (x_sum + y_sum + a_sum + b_sum + c_sum + d_sum + e_sum)):
-								f = (limit - (x_sum + y_sum + a_sum + b_sum + c_sum + d_sum + e_sum))
+							count += 1
 
-							temp_seqs = array('B', [x, y, a, b, c, d, e, f])
-							unique_seqs.append(temp_seqs)
-
-							f = 0
-
-							e += 1
-
-						d += 1
-
-					c += 1
-
-				b += 1
-
-			a += 1
-
-		y += 1
-
-	x += 1
-
-unique_seqs.pop(0)
-print(len(unique_seqs))
+print(count)
 
 
+							#if (limit - (x_sum + y_sum + a_sum + b_sum + c_sum + d_sum + e_sum)):
+								#f = (limit - (x_sum + y_sum + a_sum + b_sum + c_sum + d_sum + e_sum))
